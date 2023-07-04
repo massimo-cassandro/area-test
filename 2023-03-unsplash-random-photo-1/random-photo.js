c35329e8742b5dc4c410782cc5e66761408ddc6a
Massimo Cassandro <massimo.cassandro@gmail.com>
2023-07-04 13:45:03 +0200

    fix

--- 2023-03-unsplash-random-photo-1/random-photo.js
+++ 2023-03-unsplash-random-photo-1/random-photo.js
@@ -9,7 +9,7 @@ import { decode } from './blurhash/dist/esm/index.js';
 
   const container = document.querySelector('.container'),
     credits_container = document.querySelector('.credits'),
-    url = 'https://primominuto.altervista.org/proxy/getUnsplasPhotos.php?m=tfc4lmFw',
+    url = 'https://primominuto.altervista.org/proxy/getUnsplashPhotos.php?m=tfc4lmFw',
     debug = true; // add debug info
 
   // local testing
